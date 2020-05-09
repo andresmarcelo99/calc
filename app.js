03e46cb6858ec59624b83178cf508335f8261a74
andresmarcelo99 <marcelo.andres99.ma@gmail.com>
2020-05-09 15:41:51 -0600

    added the rest of the functions

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const calc = (n1, n2) => {
   const sum = n1 + n2;
-
-  return sum;
+  const sub = n1 - n2;
+  const mult = n1 * n2;
+  let div = 0;
+  if (n2 != 0) {
+    div = n1 / n2;
+  }
+  console.log("Sum:", sum);
+  console.log("Sub:", sub);
+  console.log("Mult:", mult);
+  console.log("Div:", div);
 };
 
-console.log(calc(1, 2));
+calc(5, 10);
